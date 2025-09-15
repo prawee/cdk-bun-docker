c7492b8d618261295fde3647130f5ac4985922ba
Prawee Wongsa <prawee@hotmail.com>
2025-09-15 19:01:00 +0700

    fix deploy on lambda

--- lib/cdk-bun-docker-stack.ts
+++ lib/cdk-bun-docker-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ecrAssets from 'aws-cdk-lib/aws-ecr-assets';
 import * as path from 'path';
 
 export class BunDockerStack extends cdk.Stack {
@@ -10,7 +11,11 @@ export class BunDockerStack extends cdk.Stack {
     const fn = new lambda.DockerImageFunction(this, `PocBunDocker`, {
       functionName: `poc-bun-docker`,
       description: 'POC Lambda with Docker',
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../lambda')),
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../lambda'), {
+        platform: ecrAssets.Platform.LINUX_ARM64,
+        file: 'Dockerfile'
+      }),
+      architecture: lambda.Architecture.ARM_64,
       memorySize: 256,
       timeout: cdk.Duration.seconds(120),
     });
